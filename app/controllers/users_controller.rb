5f2f771559324172eb3674fd38b2324f2cf2bc1a
Luis Fernando Nascimento <nascimento.lsfo@gmail.com>
2016-01-22 18:42:13 -0200

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Minitwitter!"
       redirect_to @user
     else
       render 'new'
     end
   end
+
+
 
   private
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -64,4 +64,7 @@ foo@bar_baz.com foo@bar+baz.com]
     assert_not @user.valid?
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
